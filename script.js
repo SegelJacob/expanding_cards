de2dc924d7c829f993638a53921a4cae3ac5fead
Segel Jacob <svarapathukuzhi5556@conestogac.on.ca>
2021-09-09 10:13:03 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+// puts all of the panels into a node list
+const panels = document.querySelectorAll('.panel');
+panels.forEach((panel) => {
+  panel.addEventListener('click', () => {
+    removeActiveClasses();
+    panel.classList.add('active');
+  });
+});
+
+function removeActiveClasses() {
+  panels.forEach((panel) => {
+    panel.classList.remove('active');
+  });
+}
